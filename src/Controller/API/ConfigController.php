2e83c6a48053257b179d005304e05a1823bf2a55
El <irina.elina.r@gmail.com>
2024-12-20 12:19:34 +0300

    Update ConfigController.php
    Changed config controller VERBs and data sending

--- src/Controller/API/ConfigController.php
+++ src/Controller/API/ConfigController.php
@@ -4,6 +4,7 @@ namespace App\Controller\API;
 
 use App\Repository\ConfigRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route("/config")]
@@ -18,23 +19,30 @@ class ConfigController extends AbstractController
     {
         $this->configRepository = $configRepository;
     }
-    #[Route("/tentative/{value}", methods: ["GET"])]
-    public function updateTentatite($value)
+
+    #[Route("/tentative", methods: ["POST"])]
+    public function updateTentatite(Request $request)
     {
+        $data = json_decode($request->getContent(), true);
+        $value = $data["value"];
+
         $config=$this->configRepository->findByNom("tentative");
         $config->setValeur($value);
         $this->configRepository->update($config);
-        return $this->json("Tentative changer avec succes", 200, [], []);
+        return $this->json("Tentative changée avec succes", 200, [], []);
 
     }
 
-    #[Route("/delais/{value}", methods: ["GET"])]
-    public function updatedelais($value)
+    #[Route("/delais", methods: ["POST"])]
+    public function updatedelais(Request $request)
     {
+        $data = json_decode($request->getContent(), true);
+        $value = $data["value"];
+
         $config=$this->configRepository->findByNom("delais");
         $config->setValeur($value);
         $this->configRepository->update($config);
-        return $this->json("Delais changer avec succes", 200, [], []);
+        return $this->json("Delais changée avec succes", 200, [], []);
 
     }
 
