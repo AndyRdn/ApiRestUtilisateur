7c9bed0a050aa01fe2a290c9e6c9b0250f110668
Miarantsoa Ainaharison <miarantsoaainaharison@gmail.com>
2025-01-26 13:36:26 +0300

    script and controller Utilisateur updated

--- src/Controller/API/UtilisateurController.php
+++ src/Controller/API/UtilisateurController.php
@@ -197,20 +197,21 @@ class UtilisateurController extends AbstractController
             if ($code== null){
                 $mailer->send($this->email->createMail($utilisateur->getMail(), EmailSubject::AUTHENTIFICATION->value, $utilisateur->getId()));
                 $resp = ResponseService::getJSONTemplate('success', [
-                    "message"  => "Un email de confirmation a ete envoyé"
+                    "message"  => "Un email de confirmation a été envoyé",
+                    "data" => $utilisateur->getId()
                 ]);
                 return $this->json($resp, 200, [], []);
             }else{
                 $resp = ResponseService::getJSONTemplate('error', [
-                    "message"  => "Le code envoyer précédement est encore valide"
+                    "message"  => "Le code envoyé précédement est encore valide"
                 ]);
-                return $this->json($resp, 200, [], []);
+                return $this->json($resp, 500, [], []);
             }
 
 
         }else{
 //          check si
-            if ($tentative->getTentative()==0){
+            if ($tentative->getTentative()==1){
                 //cree une email Pour le reset de la tentative
                 $mailer->send($this->email->createMail($utilisateur->getMail(), EmailSubject::RESET->value, $utilisateur->getId()));
                 $resp = ResponseService::getJSONTemplate('error', [
@@ -221,7 +222,7 @@ class UtilisateurController extends AbstractController
                 $tentative->setTentative($tentative->getTentative()-1);
                 $this->tentativeRepository->update($tentative);
                 $resp = ResponseService::getJSONTemplate('error', [
-                    "message"  => "Mot de passe Incorrecte . Tentative restante :".$tentative->getTentative()
+                    "message"  => "Mot de passe Incorrecte. Tentative restante: ".$tentative->getTentative()
                 ]);
                 return $this->json($resp, 500, [], []);
             }
@@ -312,7 +313,8 @@ class UtilisateurController extends AbstractController
         if (!empty($updatedFields)) {
             // updating the user row in table "utilisateur"
             $this->entityManager->persist($user);
-            
+
+            $this->entityManager->flush();
             // inserting a new user row for the update (at today's dateTime) in table "historique_utilisateur"
             $histoUser->makeFromUser($user, new \DateTimeImmutable());
             $this->entityManager->persist($histoUser);
